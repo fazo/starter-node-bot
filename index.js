21438e163be45db6898b5c8a42d13de6e9ffa1fc
fazo <embrace.heresy@gmail.com>
2016-03-02 13:48:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -56,8 +56,14 @@ controller.hears(['Bluetooth off!'], ['direct_mention'], function (bot, message)
 })
 
 controller.hears(['^Message Ivan: '], ['direct_mention'], function (bot, message) {
-  bot.reply(message, 'On it!')
-  request("https://maker.ifttt.com/trigger/sms_ivan/with/key/dbUpwEFGIaKw9rnOyLsRqJ", {"value1": message.text.substring(message.text.indexOf(":")+2, message.text.length)}, function(error, response, body) {console.log(response)});
+  bot.reply(message, 'On it!');
+  var options = {
+  method: 'post',
+  body: {"value1": message.text.substring(message.text.indexOf(":")+2, message.text.length)},
+  json: true,
+  url: https://maker.ifttt.com/trigger/sms_ivan/with/key/dbUpwEFGIaKw9rnOyLsRqJ
+}
+  request(options, function(error, response, body) {console.log(response)});
 })
 
 controller.hears('.*', ['mention'], function (bot, message) {
